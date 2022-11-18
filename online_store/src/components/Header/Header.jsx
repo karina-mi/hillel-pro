7b403e972cdcecd794b9e7808f8068ce7da78eb3
karinamiheenko <karina.mi.08.08@gmail.com>
2022-11-18 17:46:03 +0200

    Header changes.

--- online_store/src/components/Header/Header.jsx
+++ online_store/src/components/Header/Header.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import './Header.css'
-import {Button} from 'react-bootstrap'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Link} from 'react-router-dom'
 
 const Header = () => {
 
-  return <div className="header-logo">
-    <i className="bi bi-unity logo-header"></i>
-    <Link to={'/cart'}><i className="bi bi-cart-check"></i></Link>
+  return <div>
+    <Navbar bg="#2b2b2b" expand="lg">
+    <Container>
+      <Navbar.Brand href="products"> <Link className='logo' to={'/products'}>
+        <i className="bi bi-unity logo-header"></i>
+      </Link>{' '}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="products">Home</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="products">All Products</NavDropdown.Item>
+            <NavDropdown.Item href="smartphones">
+              Smartphones
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="laptops">
+              Laptops
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+      <Link to={'/cart'}><i className="bi bi-cart-check"></i></Link>
+    </Container>
+    </Navbar>
   </div>
+
 }
 
 export default Header
+
+
+
